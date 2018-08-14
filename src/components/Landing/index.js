57e6d734196162c89595a348e805e93a74d3eb28
Samuel Cha <sunchipssam@gmail.com>
2018-08-14 16:04:38 -0700

    Added project text to landing

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,3 +1,7 @@
 import React from 'react';
 
-export default _ => <div className="Landing">asdf</div>;
+export default _ => (
+  <div className="Landing">
+    <div className="Projects">projects</div>
+  </div>
+);
