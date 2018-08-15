a9dedfc5778561eb96cb7030e54aef18dcedd41e
Samuel Cha <sunchipssam@gmail.com>
2018-08-15 09:15:18 -0700

    Added temp construction page

--- src/client/src/components/Landing/index.js
+++ src/client/src/components/Landing/index.js
@@ -2,6 +2,43 @@ import React from 'react';
 
 export default _ => (
   <div className="Landing">
-    <div className="Projects">projects</div>
+    {/* <div className="Projects">projects</div> */}
+    {/* copied from https://github.com/samscha/track-my-jobs/blob/master/src/client/src/components/Construction.js */}
+    {/* please note: this is temporary */}
+    <div
+      className="ConstructionTitle"
+      style={{
+        margin: '10px 0',
+        textAlign: 'center',
+      }}
+    >
+      We're under construction!
+    </div>
+    <div
+      className="ConstructionMain"
+      style={{ display: 'flex', justifyContent: 'center' }}
+    >
+      <ul style={{ listStyle: 'none', margin: '0 auto', padding: '0' }}>
+        <li style={{ textAlign: 'center', margin: '20px 0' }}>
+          <a
+            className="constructionTEMP"
+            href="https://github.com/samscha/aboutme"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+        </li>
+        <li style={{ textAlign: 'center', margin: '20px 0' }}>
+          <a
+            className="constructionTEMP"
+            href="mailto:chasamuels@gmail.com"
+            rel="noopener noreferrer"
+          >
+            Email
+          </a>
+        </li>
+      </ul>
+    </div>
   </div>
 );
