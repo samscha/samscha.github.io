a6e7d00dbfe5873a6bc78606dcabbd2d6f12ef19
Samuel Cha <sunchipssam@gmail.com>
2018-10-02 12:12:58 -0700

    Updated projects

--- src/client/src/components/_Projects/projects.tmp.js
+++ src/client/src/components/_Projects/projects.tmp.js
@@ -62,4 +62,60 @@ const TrackMyJobs = {
   description: 'Track your jobs and show off your statistics',
 };
 
-export default [PicMe, TrackMyJobs, Foodggregator];
+const Jobseekr_forked = {
+  urls: [
+    { text: 'Demo', link: 'https://jobseekr-forked.herokuapp.com/' },
+    { text: 'GitHub', link: 'https://github.com/samscha/jobseekr' },
+    {
+      text: 'Blog',
+      link:
+        'https://medium.com/@samscha/adding-search-and-duplicate-checker-to-an-existing-job-seeker-app-detailed-8577a0339429',
+    },
+  ],
+  technologies: [
+    { technology: 'MongoDB' },
+    { technology: 'Express', link: 'https://expressjs.com/' },
+    { technology: 'React' },
+    { technology: 'Node.js' },
+  ],
+  title: 'Jobseekr (forked)',
+  subtitle: 'Forked repo for job seeker app',
+  description: 'Added job search and duplicate checker features',
+};
+
+const CashEnvelope = {
+  urls: [
+    { text: 'Demo', link: 'https://expo.io/@samscha/cash-envelope-native' },
+    {
+      text: 'GitHub (app)',
+      link: 'https://github.com/samscha/cash-envelope-native',
+    },
+    {
+      text: 'GitHub (server)',
+      link: 'https://github.com/samscha/cash-envelope',
+    },
+    {
+      text: 'Blog',
+      link:
+        'https://medium.com/@samscha/creating-a-postgres-java-spring-react-native-virtual-cash-envelope-app-in-depth-tutorial-9dc62af8fd3f',
+    },
+  ],
+  technologies: [
+    { technology: 'PostgreSQL' },
+    { technology: 'Spring', link: 'https://expressjs.com/' },
+    { technology: 'Java' },
+    { technology: 'Native' },
+  ],
+  title: 'Cash Envelope',
+  subtitle: 'Virtual cash envelopes',
+  description:
+    'A simple app for keeping track of finances through virtual cash envelope',
+};
+
+export default [
+  CashEnvelope,
+  Jobseekr_forked,
+  PicMe,
+  TrackMyJobs,
+  Foodggregator,
+];
