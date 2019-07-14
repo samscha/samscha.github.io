c5ec4ba3551b0b337b7f2151b98e27c24cc353f1
Samuel Cha <sunchipssam@gmail.com>
2019-07-14 17:17:38 -0500

    add netlify redirects and build dir

---
+++ build/precache-manifest.d2cf5897f28d3cdeff7bc297acfaf26c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4276a98a4db87361259692392b24b112",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8471ae251f00f485861c",
+    "url": "/static/css/main.bb7ea16f.chunk.css"
+  },
+  {
+    "revision": "3f17392878e13d48744b",
+    "url": "/static/js/2.9d34d3aa.chunk.js"
+  },
+  {
+    "revision": "8471ae251f00f485861c",
+    "url": "/static/js/main.93946946.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "19778cf9c33d0c98076aeffd7dd100aa",
+    "url": "/static/media/font-mfizz.19778cf9.ttf"
+  },
+  {
+    "revision": "4a873e5f62afe3c8a79b6250b3d477db",
+    "url": "/static/media/font-mfizz.4a873e5f.svg"
+  },
+  {
+    "revision": "4d03e427f13a893d5e9ce14ff5ed8287",
+    "url": "/static/media/font-mfizz.4d03e427.eot"
+  },
+  {
+    "revision": "64c7cf6cb98b1297cd522df93db3d0d2",
+    "url": "/static/media/font-mfizz.64c7cf6c.woff"
+  },
+  {
+    "revision": "cfd5ddd7d5d3460d991bb49a1453474f",
+    "url": "/static/media/profileImg.cfd5ddd7.png"
+  }
+]);
