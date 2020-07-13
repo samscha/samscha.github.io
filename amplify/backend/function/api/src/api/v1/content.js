0775fa978262cba2a10077f670541de5b5bcdeae
Samuel Cha <sunchipssam@gmail.com>
2020-07-13 12:58:07 -0500

    remove graphql from backend api

--- amplify/backend/function/api/src/api/v1/content.js
+++ amplify/backend/function/api/src/api/v1/content.js
@@ -1,31 +1,9 @@
 const router = require('express').Router();
 
-//TESTING ONLY
-const { API, graphqlOperation } = require('aws-amplify');
-const Amplify = require('aws-amplify');
-const awsconfig = require('./aws-exports');
-const { createSkill } = require('../../graphql/mutations');
-let id = 0;
-
-Amplify.configure(awsconfig);
-
 router
     .route('/content')
     .get(async (req, res) => {
-        const firstSkill = {
-            id: ++id + '',
-            text: `Node.js`,
-            title: `Click to learn more about Node.js`,
-            link: `https://nodejs.org/en/`,
-            icon: [`fab`, `node-js`],
-            type: `fa`,
-        };
-
-        const newSkill = await API.graphql(
-            graphqlOperation(createSkill, { input: firstSkill }),
-        );
-
-        res.json({ success: 'get content succeed', url: req.url, newSkill });
+        res.json({ success: 'get content succeed', url: req.url });
     })
     .all((req, res) => res.sendStatus(405));
 
