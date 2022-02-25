a00c27b2bc6f95ec3dff0bede2d5e43592bd9100
chasamuels <sam@chasamuels.com>
2022-02-25 15:47:22 -0600

    Add old code for ScrollToTop

--- src/components/ScrollToTop/index.jsx
+++ src/components/ScrollToTop/index.jsx
@@ -1,3 +1,22 @@
+/* old code below
+import React from 'react';
+ import { withRouter } from 'react-router-dom';
+
+ export default withRouter(
+     class extends React.Component {
+         componentDidUpdate(prevProps) {
+             if (this.props.location.pathname !== prevProps.location.pathname) {
+                 window.scrollTo(0, 0);
+             }
+         }
+
+         render() {
+             return this.props.children;
+         }
+     },
+ );
+"new" code not working below */
+
 import React, { useEffect, useRef } from 'react';
 import { useLocation } from 'react-router-dom';
 
