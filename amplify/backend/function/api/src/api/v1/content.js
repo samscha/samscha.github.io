9fe9252e1b9225c1dd83ec20243a46bd6b6a91c2
chasamuels <sam@chasamuels.com>
2022-08-26 14:04:41 -0500

    Add tmp node v

--- amplify/backend/function/api/src/api/v1/content.js
+++ amplify/backend/function/api/src/api/v1/content.js
@@ -3,7 +3,18 @@ const router = require('express').Router();
 router
     .route('/content')
     .get((req, res) => {
-        res.json({ success: 'get content succeed', url: req.url });
+        res.json({ success: 'get content succeed', url: req.url, node: process.version });
+    })
+    .all((req, res) => res.sendStatus(405));
+
+router
+    .route('/content/version')
+    .get((req, res) => {
+        if (req.headers.auth1 === 'acbd') {
+          res.json({ vers: process.version });
+        } else {
+          res.sendStatus(403);
+        }
     })
     .all((req, res) => res.sendStatus(405));
 
