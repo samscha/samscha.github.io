3a66201d7c7aeef59882f39e7ae2731e7a5da29b
Samuel Cha <sunchipssam@gmail.com>
2022-09-15 16:24:00 -0500

    Update minor pkgs (#267)
    * Update minor pkgs
    Add nvmrc
    
    * Update pkgs
    
    * Add tmp node v
    
    * Updates per cli
    
    * Remove nvmrc for now
    
    * Revert "Updates per cli"
    
    This reverts commit c8166a7f0d38a4279a57b86d1bedc8d139941d53.
    
    * Update runtime to 14.x
    
    * Temp update amplify yml
    
    * Add nvm i
    
    * Comment out nvm usage
    
    * Update yml to echo node
    
    * Update nvmrc to 16
    Update amplify yml
    
    * Update backend to node16
    
    * Add temp auth
    
    * Fix err

--- amplify/backend/function/api/src/api/v1/content.js
+++ amplify/backend/function/api/src/api/v1/content.js
@@ -1,9 +1,27 @@
 const router = require('express').Router();
 
+// TODO: move into an auth middleware
+const tempAuth = (req, res, next) => {
+  if (req.headers.auth1 === 'acbd') {
+    next();
+  }
+
+  res.sendStatus(403);
+  return;
+}
+
+
 router
     .route('/content')
     .get((req, res) => {
         res.json({ success: 'get content succeed', url: req.url });
+    })
+    .all((req, res) => res.sendStatus(405));
+
+router
+    .route('/content/version')
+    .get(tempAuth, (req, res) => {
+        res.json({ vers: process.version });
     })
     .all((req, res) => res.sendStatus(405));
 
