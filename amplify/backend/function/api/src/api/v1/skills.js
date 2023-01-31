59193c1cf6d228c90fd43e88f509202878fb8286
chasamuels <sam@chasamuels.com>
2023-01-30 20:34:46 -0600

    run pretti script

--- amplify/backend/function/api/src/api/v1/skills.js
+++ amplify/backend/function/api/src/api/v1/skills.js
@@ -3,10 +3,13 @@ const router = require('express').Router();
 // TODO: move to db
 const skills = require('../../temp-db/skills');
 
-router.route('/skills').get((req, res) => {
-  res.status(200).json(skills);
-}).all((req,res) => {
-res.sendStatus(405)
-});
+router
+  .route('/skills')
+  .get((req, res) => {
+    res.status(200).json(skills);
+  })
+  .all((req, res) => {
+    res.sendStatus(405);
+  });
 
 exports.router = router;
