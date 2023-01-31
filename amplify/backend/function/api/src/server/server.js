ae920a4ac8e720760d6136fcc2c4c3ec3f72fb98
Samuel Cha <sunchipssam@gmail.com>
2023-01-30 20:41:59 -0600

    Add prettier (#283)
    * add prettier
    add prettier script for api
    
    * remove prettierrc
    run api script again

--- amplify/backend/function/api/src/server/server.js
+++ amplify/backend/function/api/src/server/server.js
@@ -1,38 +1,37 @@
 module.exports = (app) => {
-    const server = require('http').createServer(app);
-    const debug = require('debug')('root:server');
+  const server = require('http').createServer(app);
+  const debug = require('debug')('root:server');
 
-    const onError = (error) => {
-        if (error.syscall !== 'listen') throw error;
+  const onError = (error) => {
+    if (error.syscall !== 'listen') throw error;
 
-        const bind =
-            typeof app.port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+    const bind = typeof app.port === 'string' ? 'Pipe ' + port : 'Port ' + port;
 
-        switch (error.code) {
-            case 'EACCES':
-                console.error(bind + ' requires elevated privileges');
-                process.exit(1);
-                break;
-            case 'EADDRINUSE':
-                console.error(bind + ' is already in use');
-                process.exit(1);
-                break;
-            default:
-                throw error;
-        }
-    };
+    switch (error.code) {
+      case 'EACCES':
+        console.error(bind + ' requires elevated privileges');
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        console.error(bind + ' is already in use');
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+  };
 
-    const onListening = () => {
-        const addr = server.address();
-        const bind =
-            typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+  const onListening = () => {
+    const addr = server.address();
+    const bind =
+      typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
 
-        debug('Listening on ' + bind);
-    };
+    debug('Listening on ' + bind);
+  };
 
-    server.listen(app.get('port'));
-    server.on('error', onError);
-    server.on('listening', onListening);
+  server.listen(app.get('port'));
+  server.on('error', onError);
+  server.on('listening', onListening);
 
-    return server;
+  return server;
 };
