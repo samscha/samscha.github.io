7c8e662bec8e551bcafcce943918026cddea2be6
Samuel Cha <sunchipssam@gmail.com>
2023-02-07 11:05:08 -0600

    enable contact page (#297)
    update navbar

--- src/assets/js/contact/contacts.js
+++ src/assets/js/contact/contacts.js
@@ -5,23 +5,20 @@ export default [
     target: '',
     text: 'contact@samscha.com',
     title: `Click to email Sam`,
-  },
-  {
-    href: 'https://t.me/chasamuels',
-    icon: ['fab', 'telegram-plane'],
-    text: 'chasamuels',
-    title: `Click to Telegram Sam`,
+    enabled: true,
   },
   {
     href: 'https://linkedin.com/in/chasamuels',
     icon: ['fab', 'linkedin'],
     text: 'chasamuels',
     title: `Click to go to Sam's LinkedIn`,
+    enabled: true,
   },
   {
     href: 'https://twitter.com/chasamuels',
     icon: ['fab', 'twitter'],
     text: 'chasamuels',
     title: `Click to go to Sam's Twitter`,
+    enabled: false,
   },
 ];
