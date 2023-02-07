f667e27fe04e46e0a78a95ec0f9694b9638e3663
chasamuels <sam@chasamuels.com>
2023-02-07 15:53:26 -0600

    add contacts endpoint

--- amplify/backend/function/api/src/api/v1/index.js
+++ amplify/backend/function/api/src/api/v1/index.js
@@ -1,9 +1,11 @@
+const contacts = require('./contacts');
 const location = require('./location');
 const skills = require('./skills');
 
 module.exports = [
   require('./root'),
   require('./content'),
+  contacts.router,
   location.router,
   skills.router,
 ];
---
+++ amplify/backend/function/api/src/temp-db/contacts.js
@@ -0,0 +1,24 @@
+module.exports = [
+  {
+    href: 'mailto:contact@samscha.com',
+    icon: ['fas', 'envelope'],
+    target: '',
+    text: 'contact@samscha.com',
+    title: `Click to email Sam`,
+    enabled: true,
+  },
+  {
+    href: 'https://linkedin.com/in/chasamuels',
+    icon: ['fab', 'linkedin'],
+    text: 'chasamuels',
+    title: `Click to go to Sam's LinkedIn`,
+    enabled: true,
+  },
+  {
+    href: 'https://twitter.com/chasamuels',
+    icon: ['fab', 'twitter'],
+    text: 'chasamuels',
+    title: `Click to go to Sam's Twitter`,
+    enabled: false,
+  },
+];
