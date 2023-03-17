3872136f6a83a5aac8a13f914eb6ac55b3f8d7d2
chasamuels <sam@chasamuels.com>
2023-03-17 16:05:41 -0500

    add secret via cli

--- amplify/backend/function/api/src/api/v1/content.js
+++ amplify/backend/function/api/src/api/v1/content.js
@@ -1,9 +1,38 @@
+const {
+  //SSM,
+SSMClient,
+GetParametersCommand
+} = require("@aws-sdk/client-ssm");
 const router = require('express').Router();
-const secrets = require('../../app/secrets');
+//const secrets = require('../../app/secrets');
 
 // TODO: move into an auth middleware
-const tempAuth = (req, res, next) => {
-  const authKey = secrets.get('AUTH_KEY_TEMP');
+const tempAuth = async (req, res, next) => {
+
+
+const client = new SSMClient();
+const command = new GetParametersCommand({
+      Names: ["AUTH_KEY_TEMP"].map(secretName => process.env[secretName]),
+      WithDecryption: true,
+
+});
+
+const response = await client.send(command);
+console.log('res',response);
+
+/*
+  const { Parameters } = await (new SSM())
+    .getParameters({
+      Names: ["AUTH_KEY_TEMP"].map(secretName => process.env[secretName]),
+      WithDecryption: true,
+    })
+    .promise();
+*/
+
+//const authKey = Parameters.pop().Value;
+const authKey = response.Parameters.pop().Value;
+
+  //const authKey = secrets.get('AUTH_KEY_TEMP');
 
   if (req.headers.auth1 === authKey) {
     next();
--- amplify/backend/function/api/src/index.js
+++ amplify/backend/function/api/src/index.js
@@ -1,3 +1,17 @@
+/*
+Use the following code to retrieve configured secrets from SSM:
+
+const aws = require('aws-sdk');
+
+const { Parameters } = await (new aws.SSM())
+  .getParameters({
+    Names: ["AUTH_KEY_TEMP"].map(secretName => process.env[secretName]),
+    WithDecryption: true,
+  })
+  .promise();
+
+Parameters will be of the form { Name: 'secretName', Value: 'secretValue', ... }[]
+*/
 const awsServerlessExpress = require('aws-serverless-express');
 const app = require('./awsapp');
 
