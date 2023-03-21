8a90086b83bc15985b4073e7abd0c58939b2170e
chasamuels <sam@chasamuels.com>
2023-03-21 05:06:57 -0500

    revert aws serverless update

--- amplify/backend/function/api/src/index.js
+++ amplify/backend/function/api/src/index.js
@@ -1,23 +1,9 @@
-/*
-Use the following code to retrieve configured secrets from SSM:
+ const awsServerlessExpress = require('aws-serverless-express');
+ const app = require('./awsapp');
 
-const aws = require('aws-sdk');
+ const server = awsServerlessExpress.createServer(app);
 
-const { Parameters } = await (new aws.SSM())
-  .getParameters({
-    Names: ["AUTH_KEY_TEMP"].map(secretName => process.env[secretName]),
-    WithDecryption: true,
-  })
-  .promise();
-
-Parameters will be of the form { Name: 'secretName', Value: 'secretValue', ... }[]
-*/
-const serverlessExpress = require('@vendia/serverless-express')
-
-const app = require('./awsapp');
-
-exports.handler = (event, context) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
-    //awsServerlessExpress.proxy(server, event, context);
-    serverlessExpress({ app })(event, context);
-};
+ exports.handler = (event, context) => {
+     console.log(`EVENT: ${JSON.stringify(event)}`);
+     awsServerlessExpress.proxy(server, event, context);
+ };
