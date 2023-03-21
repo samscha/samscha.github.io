6aebb7ccf471b57738d1c9ff9c571ce9d54bb678
chasamuels <sam@chasamuels.com>
2023-03-21 05:08:34 -0500

    fix more spacing

--- amplify/backend/function/api/src/index.js
+++ amplify/backend/function/api/src/index.js
@@ -1,9 +1,9 @@
- const awsServerlessExpress = require('aws-serverless-express');
- const app = require('./awsapp');
+const awsServerlessExpress = require('aws-serverless-express');
+const app = require('./awsapp');
 
- const server = awsServerlessExpress.createServer(app);
+const server = awsServerlessExpress.createServer(app);
 
- exports.handler = (event, context) => {
-     console.log(`EVENT: ${JSON.stringify(event)}`);
-     awsServerlessExpress.proxy(server, event, context);
- };
+exports.handler = (event, context) => {
+    console.log(`EVENT: ${JSON.stringify(event)}`);
+    awsServerlessExpress.proxy(server, event, context);
+};
