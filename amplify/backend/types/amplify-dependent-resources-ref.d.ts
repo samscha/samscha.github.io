d53bfad0537dfbfdcb3fb32efab3f085b84dfab7
chasamuels <sam@chasamuels.com>
2023-03-23 10:57:31 -0500

    add push iterative

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -10,6 +10,7 @@ export type AmplifyDependentResourcesAttributes = {
     "api": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
       "Name": "string",
       "Region": "string"
     }
