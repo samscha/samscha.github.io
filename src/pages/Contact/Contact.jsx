a6e88955210e7d1a948303e41b3515d75411571d
chasamuels <sam@chasamuels.com>
2023-04-03 05:58:18 -0500

    remove api calls

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,66 +1,73 @@
 import React from 'react';
-import { useQuery } from 'react-query';
+// import { useQuery } from 'react-query';
 import PropTypes from 'prop-types';
 
 import './Contact.scss';
 
-export default ({ ErrorText, IconsBy, IconText, Link, Loading }) => {
+export default ({
+  // ErrorText,
+  IconsBy,
+  IconText,
+  Link,
+  // Loading,
+  data: { contacts },
+}) => {
   const ContactPage = () => {
-    const apiBaseUri = process.env.REACT_APP_API_BASE_URL;
+    // const apiBaseUri = process.env.REACT_APP_API_BASE_URL;
 
-    const fetchContacts = async () => {
-      const response = await fetch(`${apiBaseUri}/v1/contacts`);
-      if (response.status !== 200) {
-        throw new Error(JSON.stringify(response));
-      }
+    // const fetchContacts = async () => {
+    //   const response = await fetch(`${apiBaseUri}/v1/contacts`);
+    //   if (response.status !== 200) {
+    //     throw new Error(JSON.stringify(response));
+    //   }
 
-      return response.json();
-    };
+    //   return response.json();
+    // };
 
-    const contactsQuery = useQuery('contacts', fetchContacts);
+    // const contactsQuery = useQuery('contacts', fetchContacts);
 
     return (
       <div className="contact-page">
-        {contactsQuery.isLoading && <Loading />}
+        {/* {contactsQuery.isLoading && <Loading />}
         {contactsQuery.isError && <ErrorText />}
-        {contactsQuery.isSuccess && (
-          <React.Fragment>
-            {contactsQuery.data.contacts.length > 0 ? (
-              <React.Fragment>
-                <div className="links-section">
-                  {contactsQuery.data.contacts
-                    .filter((c) => c.enabled)
-                    .map((info) => {
-                      const { href, icon, target, text, title } = info;
+        {contactsQuery.isSuccess && ( */}
+        <>
+          {contacts.length > 0 ? (
+            <>
+              <div className="links-section">
+                {contacts
+                  .filter((c) => c.enabled)
+                  .map((info) => {
+                    const { href, icon, target, text, title } = info;
 
-                      return (
-                        href &&
-                        icon &&
-                        text &&
-                        title && (
-                          <div key={href} className="link-container">
-                            <Link href={href} target={target} title={title}>
-                              <IconText fixedWidth icon={icon} text={text} />
-                            </Link>
-                          </div>
-                        )
-                      );
-                    })}
-                </div>
-
-                <IconsBy fa />
-              </React.Fragment>
-            ) : (
-              <div className="link-container">
-                <IconText
-                  fixedWidth
-                  icon={['fas', 'file-circle-question']}
-                  text="No contact info"
-                />
+                    return (
+                      href &&
+                      icon &&
+                      text &&
+                      title && (
+                        <div key={href} className="link-container">
+                          <Link href={href} target={target} title={title}>
+                            <IconText fixedWidth icon={icon} text={text} />
+                          </Link>
+                        </div>
+                      )
+                    );
+                  })}
               </div>
-            )}
-          </React.Fragment>
-        )}
+
+              <IconsBy fa />
+            </>
+          ) : (
+            <div className="link-container">
+              <IconText
+                fixedWidth
+                icon={['fas', 'file-circle-question']}
+                text="No contact info"
+              />
+            </div>
+          )}
+        </>
+        {/* )} */}
       </div>
     );
   };
@@ -79,28 +86,3 @@ export default ({ ErrorText, IconsBy, IconText, Link, Loading }) => {
 
   return ContactPage;
 };
-
-const backupContacts = [
-  {
-    href: 'mailto:contact@samscha.com',
-    icon: ['fas', 'envelope'],
-    target: '',
-    text: 'contact@samscha.com',
-    title: `Click to email Sam`,
-    enabled: true,
-  },
-  {
-    href: 'https://linkedin.com/in/chasamuels',
-    icon: ['fab', 'linkedin'],
-    text: 'chasamuels',
-    title: `Click to go to Sam's LinkedIn`,
-    enabled: true,
-  },
-  {
-    href: 'https://twitter.com/chasamuels',
-    icon: ['fab', 'twitter'],
-    text: 'chasamuels',
-    title: `Click to go to Sam's Twitter`,
-    enabled: false,
-  },
-];
