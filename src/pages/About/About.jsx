9f5eab9f1e4d088df503993c91fd8c343b629cbc
Samuel Cha <sunchipssam@gmail.com>
2023-04-03 06:35:36 -0500

    Clean up remove api (#346)
    * remove comments
    remove react-query
    
    * fix pretti

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,72 +1,37 @@
 import React from 'react';
-// import { useQuery } from 'react-query';
 
 import './About.scss';
 
-export default ({
-  // ErrorText,
-  IconsBy,
-  IconText,
-  Link,
-  // Loading,
-  infoText,
-  data: { info },
-}) => {
-  // const apiBaseUri = process.env.REACT_APP_API_BASE_URL;
-
-  // const types = ['work', 'education'];
-
+export default ({ IconsBy, IconText, Link, infoText, data: { info } }) => {
   return () => {
-    // const fetchInfo = async () => {
-    //   const typeQueryParams = types.join(',');
-    //   // TODO: add sort query params
-
-    //   const response = await fetch(
-    //     `${apiBaseUri}/v1/info?types=${typeQueryParams}`
-    //   );
-    //   if (response.status !== 200) {
-    //     throw new Error(JSON.stringify(response));
-    //   }
-
-    //   return response.json();
-    // };
-
-    // const infoQuery = useQuery('info', fetchInfo);
-
     return (
       <div className="about-page">
-        {/* {infoQuery.isLoading && <Loading />}
-        {infoQuery.isError && <ErrorText />}
-        {infoQuery.isSuccess && ( */}
-        <>
-          {info.length > 0 && (
-            <div className="infos-section">
-              {info.map((info) => {
-                const { href, icon, target, text, title } = info;
+        {info.length > 0 && (
+          <div className="infos-section">
+            {info.map((info) => {
+              const { href, icon, target, text, title } = info;
 
-                return (
-                  href &&
-                  icon &&
-                  text &&
-                  title && (
-                    <div key={href} className="link-container">
-                      <Link href={href} target={target} title={title}>
-                        <IconText fixedWidth icon={icon} text={text} />
-                      </Link>
-                    </div>
-                  )
-                );
-              })}
-            </div>
-          )}
+              return (
+                href &&
+                icon &&
+                text &&
+                title && (
+                  <div key={href} className="link-container">
+                    <Link href={href} target={target} title={title}>
+                      <IconText fixedWidth icon={icon} text={text} />
+                    </Link>
+                  </div>
+                )
+              );
+            })}
+          </div>
+        )}
 
-          {infoText && <p className="info-text">{infoText}</p>}
+        {infoText && <p className="info-text">{infoText}</p>}
 
-          {info.length > 0 && <IconsBy fa />}
+        {info.length > 0 && <IconsBy fa />}
 
-          {info.length === 0 && !infoText && <div>No info to show</div>}
-        </>
-        {/* )} */}
+        {info.length === 0 && !infoText && <div>No info to show</div>}
       </div>
     );
   };
