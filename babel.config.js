5d1a988b9cd272cd4cb4404b5083903f4b117e5b
Samuel Cha <sunchipssam@gmail.com>
2023-07-06 09:26:33 -0500

    Add ts (#363)
    * update react router dom
    
    * npm i for ci
    
    * add ts
    
    * add ts
    
    * update pkgs
    
    * npm i to fix ci
    
    * fix dev dep
    
    * update favicon
    
    * fix missing dev dep

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,8 @@
 module.exports = {
-    presets: ['@babel/preset-env', '@babel/preset-react'],
-    plugins: ['@babel/plugin-proposal-class-properties'],
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react',
+    '@babel/preset-typescript',
+  ],
+  plugins: ['@babel/plugin-proposal-class-properties'],
 };
---
+++ src/pages/About/About.test.tsx
@@ -0,0 +1,20 @@
+// import React from 'react';
+// import { render } from '@testing-library/react';
+
+// import aboutComponentFactory from './About';
+
+describe.skip(`About`, function () {
+  const baseComponentProps = {
+    IconsBy: () => {},
+    IconText: () => {},
+    Link: () => {},
+    infoText: 'info',
+    data: { info: [] },
+  };
+  // const About = aboutComponentFactory({ ...baseComponentProps });
+
+  test.skip('should redner', () => {
+    // console.log(About());
+    // render(<About />)
+  });
+});
