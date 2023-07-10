f26936b9dfe79fdc6f6af3ed687668fc00b82632
chasamuels <sam@chasamuels.com>
2023-07-10 17:12:55 -0500

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,14 @@ module.exports = {
   },
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  /*
+  "setupFilesAfterEnv": [
+    "@testing-library/jest-dom/extend-expect"
+]
+*/
 };
 
 // module.exports = {
