1ce9cdbe399b881ad3914052bec68402ea259886
Samuel Cha <sunchipssam@gmail.com>
2025-04-09 07:24:59 -0500

    add describe and it to test

--- src/components/Page/index.test.tsx
+++ src/components/Page/index.test.tsx
@@ -1,40 +1,42 @@
 import { render, screen } from '@testing-library/react';
-import { expect, test } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { Page } from '../';
 
-test('renders Page', async () => {
-  const { container } = render(<Page />);
+describe('Page', () => {
+  it('renders', async () => {
+    const { container } = render(<Page />);
 
-  expect(container.getElementsByClassName('page')).toHaveLength(1);
-});
+    expect(container.getElementsByClassName('page')).toHaveLength(1);
+  });
 
-test("doesn't render sections", async () => {
-  render(<Page />);
+  it("doesn't render sections", async () => {
+    render(<Page />);
 
-  expect(screen.queryByRole('link')).toBeNull();
-  expect(screen.queryByText('Technical Skills')).toBeNull();
-  expect(screen.queryByText('Experiences')).toBeNull();
-  expect(screen.queryByText('Projects')).toBeNull();
-  expect(screen.queryByText('Education')).toBeNull();
-});
+    expect(screen.queryByRole('link')).toBeNull();
+    expect(screen.queryByText('Technical Skills')).toBeNull();
+    expect(screen.queryByText('Experiences')).toBeNull();
+    expect(screen.queryByText('Projects')).toBeNull();
+    expect(screen.queryByText('Education')).toBeNull();
+  });
 
-test('renders header', async () => {
-  render(<Page />);
+  it('renders header', async () => {
+    render(<Page />);
 
-  const header = screen.getByText('Samuel Cha');
-  expect(header).toBeInTheDocument();
-});
+    const header = screen.getByText('Samuel Cha');
+    expect(header).toBeInTheDocument();
+  });
 
-test('renders subheader', async () => {
-  render(<Page />);
+  it('renders subheader', async () => {
+    render(<Page />);
 
-  const subheader = screen.getByText('Seattle, WA');
-  expect(subheader).toBeInTheDocument();
-});
+    const subheader = screen.getByText('Seattle, WA');
+    expect(subheader).toBeInTheDocument();
+  });
 
-test('renders footer', async () => {
-  const { container } = render(<Page />);
+  it('renders footer', async () => {
+    const { container } = render(<Page />);
 
-  const footer = container.getElementsByClassName('footer');
-  expect(footer).toHaveLength(1);
+    const footer = container.getElementsByClassName('footer');
+    expect(footer).toHaveLength(1);
+  });
 });
